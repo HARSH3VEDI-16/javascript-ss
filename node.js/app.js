74ee72388c19f5afe8e4b5ca7e270e496eeb7ed8
Harsh Trivedi <harsh.t@sarvadhi.com>
2026-02-09 17:42:41 +0530

    node.js practice

---
+++ node.js/app.js
@@ -0,0 +1,4 @@
+// app.js for require ()
+// app.js
+const add = require('./common');
+console.log(add(2, 3));
---
+++ node.js/common.js
@@ -0,0 +1,7 @@
+// common.js for module.exports
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = add;
+
